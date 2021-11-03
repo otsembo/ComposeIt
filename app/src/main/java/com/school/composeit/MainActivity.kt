8eead83a633ac905fe47f8d2e63ef39db9ecb920
otsembo <okumu.otsembo@gmail.com>
2021-11-03 23:39:25 +0300

    Added list of items

--- app/src/main/java/com/school/composeit/MainActivity.kt
+++ app/src/main/java/com/school/composeit/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -35,6 +40,12 @@ class MainActivity : ComponentActivity() {
 
 data class Message(val author:String, val body:String)
 
+val sampleConversations = listOf<Message>(
+    Message("Albert", "Theory of relativity"),
+    Message("Newton", "An apple from the tree"),
+    Message("Pythagoras", "Well that's a love triangle.")
+)
+
 
 //composable function
 @Composable
@@ -49,7 +60,8 @@ fun MessageCard(message:Message){
                 .padding(2.dp)
                 .height(125.dp)
                 .width(125.dp)
-                .clip(CircleShape),
+                .clip(CircleShape)
+                .border(2.5.dp, MaterialTheme.colors.secondary, CircleShape),
             alignment = Alignment.Center,
             contentScale = ContentScale.Crop
             )
@@ -58,10 +70,26 @@ fun MessageCard(message:Message){
         Spacer(modifier = Modifier.width(10.dp))
 
         Column {
-            Text(text = message.author)
+            Text(
+                text = message.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.h6
+            )
             //vertical space
             Spacer(modifier = Modifier.height(5.dp))
-            Text(text = message.body)
+            
+            Surface(shape = MaterialTheme.shapes.medium,
+                elevation = 2.dp, ) {
+
+                Text(
+                    text = message.body,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier.padding(5.dp)
+                )
+
+            }
+            
+
         }
     }
 
@@ -76,3 +104,58 @@ fun PreviewMessageCard(){
             "Madness is attempting to do the same thing and expecting different results!")
     )
 }
+
+
+//list of items
+@Composable
+fun SimpleConversation(messages: List<Message>){
+
+    LazyColumn{
+        items(messages){
+            message -> MessageCard(message = message)
+        }
+
+    }
+
+}
+
+//sample preview
+@Preview
+@Composable
+fun PreviewSimpleConversation(){
+    ComposeItTheme {
+        SimpleConversation(messages = sampleConversations)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
